3de4f97241a4cab5bc01974277a1f99bf410394a
kon31# <ma-ko3110@outlook.jp>
2024-01-28 14:14:38 +0900

    Person 1/28-6

--- src/jp/co/diworks/action/Person.java
+++ src/jp/co/diworks/action/Person.java
@@ -3,6 +3,7 @@ package jp.co.diworks.action;
 	public class Person {
 		public String name = null;
 		public int age = 0;
-		public String phoneNumber = null;
+		public String phoneNumber1 = null;
+		public int  phoneNumber2 = 0;
 		public String address = null;
 	}
--- src/jp/co/diworks/action/Test.java
+++ src/jp/co/diworks/action/Test.java
@@ -4,13 +4,17 @@ public class Test {
 
 	public static void main(String[] args) {
 		Person saito = new Person();
+		
 		saito.name = "斎藤誠";
 		saito.age = 31;
-		
+		saito.phoneNumber1 = "08012345678";
+		saito.phoneNumber2 = 1234567890; //変数の型をintにした状態で「0」から始まる数字だとエラーになる
+		saito.address = "東京都多摩市○○町１丁目２番１３号";
 		
 		System.out.println(saito.name);
 		System.out.println(saito.age);
-		// TODO 自動生成されたメソッド・スタブ
+		System.out.println(saito.phoneNumber1);
+		System.out.println(saito.address);
 
 	}
 
