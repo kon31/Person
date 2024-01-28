043b8806cb975223660dcb89d2fabc8741f16d84
kon31# <ma-ko3110@outlook.jp>
2024-01-28 15:12:41 +0900

    Person 1/28-8

--- src/jp/co/diworks/action/Test.java
+++ src/jp/co/diworks/action/Test.java
@@ -4,18 +4,31 @@ public class Test {
 
 	public static void main(String[] args) {
 		Person saito = new Person();
+		Robot aibo = new Robot();
+		Robot asimo = new Robot();
+		Robot papper = new Robot();
 		
 		saito.name = "斎藤誠";
 		saito.age = 31;
 		saito.phoneNumber1 = "08012345678";
 		saito.phoneNumber2 = 1234567890; //変数の型をintにした状態で「0」から始まる数字だとエラーになる
 		saito.address = "東京都多摩市○○町１丁目２番１３号";
+		aibo.name = "aibo";
+		asimo.name= "asimo";
+		papper.name = "papper"; 
 		
 		System.out.println(saito.name);
 		System.out.println(saito.age);
 		System.out.println(saito.phoneNumber1);
 		System.out.println(saito.address);
+		saito.talk();
+		saito.walk();
+		saito.run();
+		aibo.talk();
+		asimo.walk();
+		papper.run();
 
 	}
+	
 
 }
