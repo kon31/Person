3de10d1106478859d7e6cd179383a754e6f62b09
kon31# <ma-ko3110@outlook.jp>
2024-01-28 13:26:48 +0900

    Person 1/28-3

--- src/jp/co/diworks/action/Test.java
+++ src/jp/co/diworks/action/Test.java
@@ -3,13 +3,13 @@ package jp.co.diworks.action;
 public class Test {
 
 	public static void main(String[] args) {
-		Person jiro = new Person();
-		jiro.name = "木村次郎";
-		jiro.age = 18;
+		Person hanako = new Person();
+		hanako.name = "鈴木花子";
+		hanako.age = 16;
 		
 		
-		System.out.println(jiro.name);
-		System.out.println(jiro.age);
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
